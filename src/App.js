4e8226eda7fecce7e956cd7d446e720b35a6a988
JOAO DE ASSIS FORLANO <CG303805X@car.ifsp>
2026-03-24 17:25:07 -0300

    aprendendo map

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import Header from './components/Header';
-import FormSignUp from './components/FormSignUp';
+import CardUser from './components/CardUser';
+import { useEffect } from 'react';
 
 export default function App() {
+
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+useEffect(() => {
+    const getUsers = async () => {
+      const response = await fetch('http://localhost:3333/user');
+      const data = await response.json();
+      if (response.ok) {
+        console.log(data.users);
+        setUsers(data.users);
+      } else {
+        console.error("Erro ao buscar usuários:", data);
+      }
+    }
+    getUsers();
+}, [])
+
   return (
       <View style={styles.container}>
         <Header />
-        <FormSignUp />
+
+
+        {users.map(user => (
+        <CardUser 
+          key={user.id}
+          avatar={user.avatar}
+          name={user.name}
+          email={user.email}
+        />
+        ))}
         <StatusBar style="auto" />
       </View>
   );
@@ -16,6 +44,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#f7f7f7'
+    backgroundColor: '#f7f7f7',
+    alignItems: 'center',
+    //justifyContent: 'center',
   }
 });
